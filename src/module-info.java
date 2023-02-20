f81b7c9bccd0f2b09a1ea0bb9c463820a4462ba7
NitinFaircent <nitin.more@faircent.com>
2023-02-20 18:05:08 +0530

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Nitin.More
+ *
+ */
+module Githubdemo {
+}
---
+++ src/pkggithub/testv1.java
@@ -0,0 +1,21 @@
+package pkggithub;
+
+public class testv1 {
+
+	public static void main(String[] args)
+	{
+		String name ="faircent";
+		
+		if(name =="faircent")
+		{
+			System.out.println("test pass");
+			
+		}
+		else
+		{
+			System.out.println("test fail");
+		}
+
+	}
+
+}
